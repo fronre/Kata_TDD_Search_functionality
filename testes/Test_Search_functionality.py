f7f2df105e07cb68c830302eec5e8dfd17262603
fronre <islamhala35@gmail.com>
2025-11-02 20:55:59 +0100

    refactoring

--- testes/Test_Search_functionality.py
+++ testes/Test_Search_functionality.py
@@ -1,21 +1,16 @@
+import pytest
 from src.Search_functionality import search_cities
 
-def test_search_with_different_case_should_pass_now():
-    assert search_cities("Ba") == ["Bangkok", "Dubai"]
-
-def test_search_text_inside_word_should_pass_now():
-    assert search_cities("gko") == ["Bangkok"]
-
-def test_search_no_match_returns_empty():
-    assert search_cities("zzz") == []
-
-def test_search_match_at_end_of_word():
-    assert search_cities("ris") == ["Paris"]
-
-def test_search_with_asterisk_returns_all_cities():
-
-    assert search_cities("*") == [
+@pytest.mark.parametrize("search_text, expected", [
+    ("Ba", ["Bangkok", "Dubai"]),
+    ("gko", ["Bangkok"]),
+    ("zzz", []),
+    ("ris", ["Paris"]),
+    ("*", [
         "Paris", "Budapest", "Skopje", "Rotterdam", "Valencia", "Vancouver",
         "Amsterdam", "Vienna", "Sydney", "New York City", "London",
         "Bangkok", "Hong Kong", "Dubai", "Rome", "Istanbul"
-    ]
+    ])
+])
+def test_search_cities(search_text, expected):
+    assert search_cities(search_text) == expected
