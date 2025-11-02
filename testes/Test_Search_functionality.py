90c7074fb72668ecbfcf925eddd29d0236993333
fronre <islamhala35@gmail.com>
2025-11-02 20:26:46 +0100

    test_search_text_inside_word_should_fail_initially

--- testes/Test_Search_functionality.py
+++ testes/Test_Search_functionality.py
@@ -1,4 +1,7 @@
 from src.Search_functionality import search_cities
 
 def test_search_with_different_case_should_fail_initially():
-    assert search_cities("Ba") == ["barchalna", "barsd", "bars"]
+    assert search_cities("Ba") == ["barcalona", "barsd", "bars"]
+
+def test_search_text_inside_word_should_fail_initially():
+    assert search_cities("can") == ["barcelona"]
