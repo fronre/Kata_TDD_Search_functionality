3667d675d92818520934880642d423b22f01c0f7
fronre <islamhala35@gmail.com>
2025-11-02 20:40:45 +0100

    test_search_no_match_returns_empty

--- testes/Test_Search_functionality.py
+++ testes/Test_Search_functionality.py
@@ -1,7 +1,10 @@
 from src.Search_functionality import search_cities
 
 def test_search_with_different_case_should_fail_initially():
-    assert search_cities("Ba") == ["barcalona", "barsd", "bars"]
+    assert search_cities("Ba") == ["barcelona", "barsd", "bars"]
 
 def test_search_text_inside_word_should_fail_initially():
-    assert search_cities("can") == ["barcelona"]
+    assert search_cities("cel") == ["barcelona"]
+
+def test_search_no_match_returns_empty():
+    assert search_cities("zzz") == []
